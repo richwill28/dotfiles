c5af3a4d117bfc7fb48b82af07ebb49536fb3754
richwill28 <e0550368@u.nus.edu>
2024-09-12 13:56:44 +0800

    Update mpv configs

--- mpv/scripts/on_top_only_while_playing.lua
+++ mpv/scripts/on_top_only_while_playing.lua
@@ -10,29 +10,29 @@
 was_ontop = mp.get_property_native("ontop")
 
 function f_load(event)
-	if was_ontop then
-		mp.set_property_native("ontop", true)
-	end
+    if was_ontop then
+        mp.set_property_native("ontop", true)
+    end
 end
 
 mp.register_event("file-loaded", f_load)
 
 mp.observe_property("pause", "bool", function(name, value)
 
-	local ontop = mp.get_property_native("ontop")
-	local is_idle = mp.get_property_native("playback-abort")
+    local ontop = mp.get_property_native("ontop")
+    local is_idle = mp.get_property_native("playback-abort")
 
-	if value then
-		mp.set_property_native("ontop", false)
-		was_ontop = ontop
-	else
-		if is_idle then
-			mp.set_property_native("ontop", false)
-			was_ontop = ontop
-		else
-			if was_ontop then
-				mp.set_property_native("ontop", true)
-			end
-		end
-	end
+    if value then
+        mp.set_property_native("ontop", false)
+        was_ontop = ontop
+    else
+        if is_idle then
+            mp.set_property_native("ontop", false)
+            was_ontop = ontop
+        else
+            if was_ontop then
+                mp.set_property_native("ontop", true)
+            end
+        end
+    end
 end)
